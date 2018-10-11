b516fb4bab392019b304e43aa6fd216bbfbb07ed
tom <t.vitureau@purjus.fr>
2018-10-11 13:10:22 +0200

    add Configuration test

---
+++ tests/Bridge/Symfony/Bundle/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Botilka\Tests\Bridge\Symfony\Bundle\DependencyInjection;
+
+use Botilka\Bridge\Symfony\Bundle\DependencyInjection\Configuration;
+use Botilka\Infrastructure\Doctrine\EventStoreDoctrine;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends TestCase
+{
+    /** @var ConfigurationInterface */
+    private $configuration;
+
+    /** @var Processor */
+    private $processor;
+
+    public function setUp()
+    {
+        $this->configuration = new Configuration();
+        $this->processor = new Processor();
+    }
+
+    public function testGetConfigTreeBuilder()
+    {
+        $treeBuilder = $this->configuration->getConfigTreeBuilder();
+        $config = $this->processor->processConfiguration($this->configuration, []);
+
+        $this->assertInstanceOf(ConfigurationInterface::class, $this->configuration);
+        $this->assertInstanceOf(TreeBuilder::class, $treeBuilder);
+
+        $expected = [
+            'event_store' => EventStoreDoctrine::class,
+            'default_messenger_config' => true,
+            'doctrine_transaction_middleware' => true,
+            'api_platform' => [
+                'expose_cq' => true,
+                'expose_event_store' => true,
+            ],
+        ];
+
+        $this->assertSame($expected, $config);
+    }
+}
