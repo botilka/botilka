631f9b00d7686d1178455a03a765139ef81a6d5a
tom <t.vitureau@purjus.fr>
2018-10-15 22:55:53 +0200

    add BotilkaReplayCommandTest

--- tests/Ui/Console/BotilkaReplayCommandTest.php
+++ tests/Ui/Console/BotilkaReplayCommandTest.php
@@ -3,27 +3,33 @@
 namespace Botilka\Tests\Ui\Console;
 
 use Botilka\Event\EventReplayerInterface;
-use PHPUnit\Framework\TestCase;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Tester\CommandTester;
+use Botilka\Ui\Console\BotilkaReplayCommand;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
-class BotilkaReplayCommandTest extends TestCase
+class BotilkaReplayCommandTest extends KernelTestCase
 {
-    public function testExecute()
+    /** @dataProvider executeProvider */
+    public function testExecute(string $id, ?int $from, ?int $to)
     {
-        $this->markTestSkipped('Not yet implemented');
-//        $kernel = static::bootKernel();
-//        $application = new Application($kernel);
-//
-//        $container = self::$container;
-//        $eventReplayer = $this->createMock(EventReplayerInterface::class);
-//        $container->set(EventReplayerInterface::class, $eventReplayer);
-//
-//        $command = $application->find('botilka:replay');
-//        $commandTester = new CommandTester($command);
-//        $commandTester->execute([
-//            'command' => $command->getName(),
-//            'id' => 'foo',
-//        ]);
+        $replayer = $this->createMock(EventReplayerInterface::class);
+        $command = new BotilkaReplayCommand($replayer);
+
+        $replayer->expects($this->once())
+            ->method('replay')
+            ->with(...[$id, $from, $to]);
+
+        $input = new ArrayInput(['id' => $id, '--from' => $from, '--to' => $to]);
+        $command->run($input, new BufferedOutput());
+    }
+
+    public function executeProvider(): array
+    {
+        return [
+            ['foo', null, null],
+            ['bar', 1, null],
+            ['baz', 1, 10],
+        ];
     }
 }
