f638b0022d75b702bc83881a09043a837ba79f6e
tom <t.vitureau@purjus.fr>
2018-11-07 23:54:01 +0100

    flatten query parameters

--- src/Bridge/ApiPlatform/Swagger/SwaggerResourcePayloadNormalizer.php
+++ src/Bridge/ApiPlatform/Swagger/SwaggerResourcePayloadNormalizer.php
@@ -4,20 +4,39 @@ namespace Botilka\Bridge\ApiPlatform\Swagger;
 
 final class SwaggerResourcePayloadNormalizer implements SwaggerResourcePayloadNormalizerInterface
 {
-    // @todo normalize array
     public function normalize(array $payload): array
     {
         $parameters = [];
 
         foreach ($payload as $name => $type) {
-            $parameters[] = [
-                'name' => $name,
-                'in' => 'query',
-                'required' => '?' !== $type[0],
-                'type' => \str_replace('?', '', $type),
-            ];
+            if (!\is_array($type)) {
+                $parameters[] = [
+                    'name' => $name,
+                    'in' => 'query',
+                    'required' => '?' !== $type[0],
+                    'type' => \str_replace(['?', 'int', 'bool'], ['', 'integer', 'boolean'], $type),
+                ];
+            } else {
+                $this->extractFromArray($name, $type, $parameters);
+            }
         }
 
         return $parameters;
+    }
+
+    private function extractFromArray(string $groupName, array $toFlatten, array &$parameters): void
+    {
+        foreach ($toFlatten as $name => $type) {
+            if (!\is_array($type)) {
+                $parameters[] = [
+                    'name' => $groupName.'['.$name.']',
+                    'in' => 'query',
+                    'required' => '?' !== $type[0],
+                    'type' => \str_replace(['?', 'int', 'bool'], ['', 'integer', 'boolean'], $type),
+                ];
+            } else {
+                $this->extractFromArray($groupName.'['.$name.']', $type, $parameters);
+            }
+        }
     }
 }
