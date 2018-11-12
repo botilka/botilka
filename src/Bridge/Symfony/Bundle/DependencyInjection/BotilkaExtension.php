de0e90fcb052ede82138629440d0dd2d395f21c6
tom <t.vitureau@purjus.fr>
2018-11-12 01:52:02 +0100

    revert BotilkaExtension.php

--- src/Bridge/Symfony/Bundle/DependencyInjection/BotilkaExtension.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/BotilkaExtension.php
@@ -127,13 +127,12 @@ final class BotilkaExtension extends Extension implements PrependExtensionInterf
             }
         }
 
-        switch ($config['event_store']) {
-            case 'Botilka\\Infrastructure\\Doctrine\\EventStoreDoctrine':
-                $loader->load('event_store_doctrine.yaml');
-                break;
-            case '\'Botilka\\Infrastructure\\MongoDB\\EventStoreMongoDB\'':
-                $loader->load('event_store_mongodb.yaml');
-                break;
+        if ('Botilka\\Infrastructure\\Doctrine\\EventStoreDoctrine' === $config['event_store']) {
+            $loader->load('event_store_doctrine.yaml');
+        }
+
+        if ('Botilka\\Infrastructure\\MongoDB\\EventStoreMongoDB' === $config['event_store']) {
+            $loader->load('event_store_mongodb.yaml');
         }
     }
 }
