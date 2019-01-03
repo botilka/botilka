40045ec29d2d73ae9205734795881b298d7ee32f
Parmentier Raphael <alunys@users.noreply.github.com>
2019-01-03 18:26:00 +0100

    fix retrieving from the envelope

--- src/Infrastructure/Symfony/Messenger/MessengerCommandBus.php
+++ src/Infrastructure/Symfony/Messenger/MessengerCommandBus.php
@@ -8,6 +8,7 @@ use Botilka\Application\Command\Command;
 use Botilka\Application\Command\CommandBus;
 use Botilka\Application\Command\CommandResponse;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 
 final class MessengerCommandBus implements CommandBus
 {
@@ -20,8 +21,11 @@ final class MessengerCommandBus implements CommandBus
 
     public function dispatch(Command $message): CommandResponse
     {
-        $evenlope = $this->bus->dispatch($message);
+        $envelope = $this->bus->dispatch($message);
 
-        return $evenlope->getMessage();
+        /** @var HandledStamp $handledStamp */
+        $handledStamp = $envelope->all(HandledStamp::class)[0];
+
+        return $handledStamp->getResult();
     }
 }
